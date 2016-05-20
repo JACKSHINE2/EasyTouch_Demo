dc3dd843720dac1ceb660fc502625a549100779c
赵光辉 <972833647@qq.com>
2016-05-20 20:56:22 +0800

    EasyTouch
    这是一个关于如何使用EasyTouch的示例工程，有自己的实现，便于以后使用查看，以此备份。

---
+++ Assets/EasyTouchBundle/EasyTouch/Example/UnityUI/UICompatibility/ETWindow.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ETWindow : MonoBehaviour {
+
+	private bool drag = false;
+
+	void OnEnable(){
+        EasyTouch.SetUICompatibily (false);
+        EasyTouch.On_TouchDown += On_TouchDown;
+		EasyTouch.On_TouchStart += On_TouchStart;
+	}
+	
+	void OnDestroy(){
+		EasyTouch.On_TouchDown -= On_TouchDown;
+		EasyTouch.On_TouchStart -= On_TouchStart;
+	}
+
+	void On_TouchStart (Gesture gesture){
+
+		drag = false;
+		if (gesture.isOverGui){
+
+			if (gesture.pickedUIElement == gameObject || gesture.pickedUIElement.transform.IsChildOf( transform)){
+				transform.SetAsLastSibling();
+				drag = true;
+
+                Debug.Log (gesture.pickedUIElement.name);
+			}
+		}
+
+
+	}
+
+	void On_TouchDown (Gesture gesture){
+
+		if (gesture.isOverGui){
+			if ((gesture.pickedUIElement == gameObject || gesture.pickedUIElement.transform.IsChildOf( transform)) && drag){
+				transform.position += (Vector3)gesture.deltaPosition;
+               
+            }
+		}
+	}
+}
---
+++ Assets/MyFirstTouch.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyFirstTouch : MonoBehaviour
+{
+    public void OnDestroy ()
+    {
+        EasyTouch.On_TouchStart -= On_TouchStart;
+    }
+
+    void Init ()
+    {
+        //EasyTouch.AddCamera (GameObject.FindGameObjectWithTag ("CameraShade").GetComponent<Camera>(), false);
+        //EasyTouch.AddCamera (GameObject.FindGameObjectWithTag ("MainCamera").GetComponent<Camera>(), false);
+    }
+
+    public void OnEnable ()
+    {
+        Init ();
+        EasyTouch.On_TouchStart += On_TouchStart;
+    }
+
+    public void OnDisable ()
+    {
+        EasyTouch.On_TouchStart -= On_TouchStart;
+    }
+
+    public void On_TouchStart (Gesture gesture)
+    {
+        if (gesture.pickedObject == null)
+        {
+            return;
+        }
+ 
+        Debug.Log ("Touch in pickedObject.name = " + gesture.pickedObject.name);
+    }
+}
